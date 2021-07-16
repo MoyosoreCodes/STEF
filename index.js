8d3127f828f491ad96afa054279d1b9492c96bbe
Moyosore <31387340+MoyosoreCodes@users.noreply.github.com>
2021-07-16 17:22:04 +0100

    make it better

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const MongodbStore = require('connect-mongodb-session')(session)
 dotenv.config();
 
 const port = process.env.PORT || 7000;
-const dbUri = "mongodb+srv://Moyosore:Moyosore12@cluster0.ky9jk.mongodb.net/medic?retryWrites=true&w=majority"
+const dbUri = "mongodb+srv://Moyosore:Moyosore12@cluster0.ky9jk.mongodb.net/STEF?retryWrites=true&w=majority"
 
 const store = new MongodbStore({
     uri: dbUri,

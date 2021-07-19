0a380d53634e155522ca77a4b08bf27cdc6a69c5
Moyosore <31387340+MoyosoreCodes@users.noreply.github.com>
2021-07-20 00:00:45 +0100

    form test

--- routes/client/appointments.js
+++ routes/client/appointments.js
@@ -2,6 +2,7 @@ const express = require('express');
 const  router = express.Router();
 const appointmentController = require('../../controller/appointmentController');
 const userController = require('../../controller/userController');
+const { User } = require('../../database/userDB');
 
 router.post('/create', async (req, res) => {console.log('here');
         const _id =  req.session.passport.user;

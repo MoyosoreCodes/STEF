0d2d8f538b1fce83de504f1fa4e160666b28d6fe
Moyosore <31387340+MoyosoreCodes@users.noreply.github.com>
2021-07-19 23:13:52 +0100

    form test

--- config/passport-config.js
+++ config/passport-config.js
@@ -11,7 +11,7 @@ passport.use(new LocalStrategy({
         async function( req, username, password, done) {
             // const patientId = username;
             let query 
-            if(patientId.includes('@')){
+            if(username.includes('@')){
                 query = {
                     'email':username
                 }
